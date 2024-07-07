1795332ad9356056f6db57d46437cb52b1bc5f95
Jakov Mitrovski <jakovvmitrovski@yahoo.com>
2024-07-07 16:28:03 +0200

    Document functions

--- src/bitcoin_utils.rs
+++ src/bitcoin_utils.rs
@@ -14,6 +14,16 @@ use bitcoin::{
 
 use bitcoincore_rpc::{Auth, Client, RawTx, RpcApi};
 
+/// This function creates an unspendable internal key. The internal key is created
+/// by generating a random secret key and deriving the corresponding public key.
+///
+/// # Arguments
+///
+/// * `secp` - A secp256k1 context of type `bitcoin::secp256k1::Secp256k1<All>`
+///
+/// # Returns
+///
+/// * `XOnlyPublicKey` - An unspendable internal key
 pub fn create_unspendable_internal_key(secp: &Secp256k1<All>) -> XOnlyPublicKey {
     let secret_key =
         bitcoin::secp256k1::SecretKey::new(&mut bitcoin::secp256k1::rand::thread_rng());
@@ -22,6 +32,19 @@ pub fn create_unspendable_internal_key(secp: &Secp256k1<All>) -> XOnlyPublicKey 
     x_only_pubkey
 }
 
+/// This function initializes a Bitcoin RPC client.
+/// The function creates a new RPC client using the provided RPC user, RPC password,
+/// and RPC URL. The function returns the initialized RPC client
+///
+/// # Arguments
+///
+/// * `rpc_user` - The RPC user of the Bitcoin Core node
+/// * `rpc_pass` - The RPC password of the Bitcoin Core node
+/// * `rpc_url` - The RPC URL of the Bitcoin Core node
+///
+/// # Returns
+///
+/// * `Client` - An initialized RPC client
 pub fn init_rpc_client(
     rpc_user: String,
     rpc_pass: String,
@@ -31,6 +54,19 @@ pub fn init_rpc_client(
     Ok(rpc)
 }
 
+/// This function initializes a Bitcoin wallet.
+/// If the wallet exists, the function loads the existing wallet.
+/// If the wallet doesn't exist, the function creates a new wallet using the provided wallet name and network
+/// and generates 101 blocks to fund the wallet.
+///
+/// # Arguments
+///
+/// * `rpc` - A Bitcoin RPC client of type `bitcoincore_rpc::Client`
+/// * `network` - The Bitcoin network for which the wallet is created. This is of type `Network`
+/// * `wallet` - The name of the wallet to create or load
+///
+/// # Returns
+/// * `(Address, Option<Transaction>, u32)` - A tuple containing the wallet address, the coinbase transaction, and the vout index
 pub fn init_wallet(
     rpc: &Client,
     network: Network,
@@ -73,6 +109,20 @@ pub fn init_wallet(
     Ok((address, coinbase_tx, 0))
 }
 
+/// This function tests and submits a set of transactions to the Bitcoin network.
+/// The function tests the transactions for mempool acceptance and submits them to the network.
+/// If the transactions are not accepted by the mempool, the function prints an error message.
+/// If the transactions are accepted, the function prints the transaction IDs and the mined block.
+///
+/// # Arguments
+///
+/// * `rpc` - A Bitcoin RPC client of type `bitcoincore_rpc::Client`
+/// * `txs` - A vector of transactions of type `Transaction`
+/// * `miner_address` - The address to which the block reward is sent, of type `Address`
+///
+/// # Returns
+///
+/// * `()` - The function returns nothing
 pub fn test_and_submit(
     rpc: &Client,
     txs: Vec<transaction::Transaction>,
@@ -118,10 +168,33 @@ pub fn test_and_submit(
     }
 }
 
+/// This function generates a new private key from a seed and a network.
+///
+/// # Arguments
+///
+/// * `seed` - The seed value used to generate the private key
+/// * `network` - The Bitcoin network for which the private key is generated. This is of type `Network`
+///
+/// # Returns
+///
+/// * `Xpriv` - A private key of type `Xpriv`
 pub fn get_private_key(seed: usize, network: Network) -> Xpriv {
     Xpriv::new_master(network, &[seed.try_into().unwrap()]).unwrap()
 }
 
+/// The function creates a change output for a transaction by calculating the change amount
+/// and generating a new address for the change output.
+///
+/// # Arguments
+///
+/// * `rpc` - A Bitcoin RPC client of type `bitcoincore_rpc::Client`
+/// * `input_info` - A vector of `OutPoint` objects representing the UTXOs to spend
+/// * `amount_to_send` - The amount of Bitcoin to send, of type `Amount`
+/// * `fee` - The fee to pay for the transaction, of type `Amount`
+///
+/// # Returns
+///
+/// * `TxOut` - A change output of type `TxOut`
 pub fn create_change_txout(
     rpc: &Client,
     input_info: &Vec<OutPoint>,
@@ -148,10 +221,20 @@ pub fn create_change_txout(
     })
 }
 
+/// Signs a transaction using the wallet's private keys.
+/// This function takes an unsigned transaction and signs it using the wallet's private keys.
+/// The function returns the signed transaction.
+/// If the transaction cannot be signed, the function panics.
+///
+/// # Arguments
+///
+/// * `rpc` - A Bitcoin RPC client of type `bitcoincore_rpc::Client`
+/// * `unsigned_tx` - An unsigned transaction of type `Transaction`
+///
+/// # Returns
+///
+/// * `Transaction` - A signed transaction
 pub fn sign_transaction_safe(rpc: &Client, unsigned_tx: Transaction) -> Transaction {
-    print!("Signing transaction...");
-    print!("{:#?}", unsigned_tx);
-
     let signed_raw_transaction = rpc
         .sign_raw_transaction_with_wallet(&unsigned_tx, None, None)
         .unwrap();
@@ -162,6 +245,24 @@ pub fn sign_transaction_safe(rpc: &Client, unsigned_tx: Transaction) -> Transact
     signed_raw_transaction.transaction().unwrap()
 }
 
+/// Commits arbitrary data to the blockchain.
+/// This function commits arbitrary data to the blockchain by creating a transaction
+/// that sends a specified amount of Bitcoin to a script that contains the data.
+/// The function returns the signed transaction, the script containing the data, and
+/// the Taproot spend information.
+///
+/// # Arguments
+///
+/// * `rpc` - A Bitcoin RPC client of type `bitcoincore_rpc::Client`
+/// * `input_info` - A vector of `OutPoint` objects representing the UTXOs to spend
+/// * `amount_to_send` - The amount of Bitcoin to send, of type `Amount`
+/// * `change` - The change output of type `TxOut`
+/// * `data` - The arbitrary data to commit, as a byte slice
+/// * `secp` - A secp256k1 context of type `bitcoin::secp256k1::Secp256k1<All
+///
+/// # Returns
+///
+/// * `(Transaction, ScriptBuf, TaprootSpendInfo)` - A tuple containing the signed transaction, the script containing the data, and the Taproot spend information
 pub fn commit_arbitrary_data(
     rpc: &Client,
     input_info: Vec<OutPoint>,
@@ -221,6 +322,21 @@ pub fn commit_arbitrary_data(
     )
 }
 
+/// Collects a specified amount of Bitcoin from the wallet.
+/// This function collects a specified amount of Bitcoin from the wallet by selecting
+/// a set of unspent outputs (UTXOs) that sum up to the desired amount. The function
+/// returns a vector of `OutPoint` objects that represent the selected UTXOs.
+/// If the wallet does not have enough funds, the function returns an error.
+///
+/// # Arguments
+///
+/// * `rpc` - A Bitcoin RPC client of type `bitcoincore_rpc::Client`
+/// * `amount` - The amount of Bitcoin to collect, of type `Amount`
+/// * `fee` - The fee to pay for the transaction, of type `Amount`
+///
+/// # Returns
+///
+/// * `Vec<OutPoint>` - A vector of `OutPoint` objects representing the selected UTXOs
 pub fn collect_amount(
     rpc: &Client,
     amount: Amount,
@@ -261,6 +377,19 @@ pub fn collect_amount(
     }
 }
 
+/// Converts a given secp256k1 private key to a Bitcoin address for a specified network.
+///
+/// This function takes a secp256k1 private key and a network identifier, and returns
+/// the corresponding Pay-to-PubKey-Hash (P2PKH) Bitcoin address for that network.
+///
+/// # Arguments
+///
+/// * `secp` - A secp256k1 context of type `bitcoin::secp256k1::Secp256k1<All>`
+/// * `private_key` - A secp256k1 private key of type `bitcoin::bip32::Xpriv`
+/// * `network` - The Bitcoin network for which the address is generated. This is of type `Network`.
+///
+/// # Returns
+/// * `Address` - A P2PKH Bitcoin address for the specified network.
 pub fn get_address_from_private_key(
     secp: &Secp256k1<All>,
     private_key: &Xpriv,
@@ -270,12 +399,38 @@ pub fn get_address_from_private_key(
     get_address_from_public_key(receiver_pubkey, network)
 }
 
+/// Converts a given secp256k1 public key to a Bitcoin address for a specified network.
+///
+/// This function takes a secp256k1 public key and a network identifier, and returns
+/// the corresponding Pay-to-PubKey-Hash (P2PKH) Bitcoin address for that network.
+///
+/// # Arguments
+///
+/// * `pk` - A secp256k1 public key of type `bitcoin::secp256k1::PublicKey`
+/// * `network` - The Bitcoin network for which the address is generated. This is of type `Network`.
+///
+/// # Returns
+/// * `Address` - A P2PKH Bitcoin address for the specified network.
 pub fn get_address_from_public_key(pk: bitcoin::secp256k1::PublicKey, network: Network) -> Address {
     let btc_pubkey = bitcoin::PublicKey::new(pk);
     Address::p2pkh(btc_pubkey, network)
 }
 
-// Function to reveal the script in the witness field and create the final transaction
+/// This function reveals arbitrary data that was previously committed to the blockchain
+/// using the `commit_arbitrary_data` function. The function creates a transaction that
+/// reveals the arbitrary data by spending the output that contains the data.
+/// The function returns the transaction that reveals the data.
+///
+/// # Arguments
+///
+/// * `prev_outpoint` - The OutPoint of the output that contains the arbitrary data
+/// * `output` - The TxOut that specifies the amount and the recipient of the revealed data
+/// * `script` - The script that contains the arbitrary data, of type `ScriptBuf`
+/// * `taproot_spend_info` - The Taproot spend information that was used to commit the data
+///
+/// # Returns
+///
+/// * `Transaction` - A transaction that reveals the arbitrary data
 pub fn reveal_arbitrary_data(
     prev_outpoint: OutPoint,
     output: TxOut,
@@ -306,6 +461,23 @@ pub fn reveal_arbitrary_data(
     unsigned_tx
 }
 
+/// This function writes arbitrary data to the blockchain by committing the data
+/// and then revealing it. The function creates two transactions: the first transaction
+/// commits the data by sending a specified amount of Bitcoin to a script that contains
+/// the data, and the second transaction reveals the data by spending the output that
+/// contains the data. The function returns the two transactions.
+///
+/// # Arguments
+///
+/// * `rpc` - A Bitcoin RPC client of type `bitcoincore_rpc::Client`
+/// * `amount_to_send` - The amount of Bitcoin to send, of type `Amount`
+/// * `fee` - The fee to pay for the transaction, of type `Amount`
+/// * `data` - The arbitrary data to commit, as a string
+/// * `receiver_address` - The Bitcoin address of the recipient of the output associated with the reveal transaction
+///
+/// # Returns
+///
+/// * `(Transaction, Transaction)` - A tuple containing the commit transaction and the reveal transaction
 pub fn write_arbitrary_data(
     rpc: &Client,
     amount_to_send: Amount,
--- src/ipc_lib.rs
+++ src/ipc_lib.rs
@@ -7,6 +7,26 @@ use crate::{
     utils,
 };
 
+/// Creates a child subnet by attaching arbitrary data to a Bitcoin transaction.
+///
+/// This function creates a Bitcoin transaction that includes specified arbitrary data and
+/// submits it to the Bitcoin network. The transaction involves creating and revealing
+/// a script containing the data using the Taproot script-path. This process ensures
+/// the data is embedded in the blockchain.
+///
+/// # Arguments
+///
+/// * `subnet_address` - A reference to a `bitcoin::Address` that represents the subnet's multisig address.
+/// * `subnet_data` - A string slice that holds the data to be embedded in the transaction. This data should contain:
+///     - A known tag indicating the creation of a new IPC Subnet.
+///     - The subnet name.
+///     - Any additional arbitrary data.
+///
+/// # Returns
+///
+/// This function returns a `Result`:
+/// * `Ok(())` - If the transaction is successfully created and submitted.
+/// * `Err(Box<dyn std::error::Error>)` - If an error occurs during the process.
 pub fn create_child(
     subnet_address: &bitcoin::Address,
     subnet_data: &str,
@@ -29,6 +49,26 @@ pub fn create_child(
     Ok(())
 }
 
+/// Joins an existing subnet by attaching validator data to a Bitcoin transaction.
+///
+/// This function creates a Bitcoin transaction that includes specified validator data
+/// and submits it to the Bitcoin network. The transaction involves creating and revealing
+/// a script containing the data using the Taproot script-path. This process ensures
+/// the data is embedded in the blockchain.
+///
+/// # Arguments
+///
+/// * `subnet_address` - A reference to a `bitcoin::Address` that represents the subnet's multisig address.
+/// * `collateral` - An `Amount` representing the collateral to be locked by the subnet's multisig address.
+/// * `validator_data` - A string slice that holds the validator data to be embedded in the transaction.
+///   This data should contain:
+///     - Validator's information, such as their IP, for discovery by other validators.
+///
+/// # Returns
+///
+/// This function returns a `Result`:
+/// * `Ok(())` - If the transaction is successfully created and submitted.
+/// * `Err(JoinChildError)` - If an error occurs during the process.
 pub fn join_child(
     subnet_address: &bitcoin::Address,
     collateral: Amount,
