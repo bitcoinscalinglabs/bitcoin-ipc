9c0572810e6a5162c662e12f73b79bb456802e43
rista404 <ristic@risticnikola.com>
2024-12-17 12:41:13 +0200

    share config with rpc handlers

--- src/bin/provider.rs
+++ src/bin/provider.rs
@@ -6,19 +6,38 @@ use std::sync::Arc;
 
 use bitcoincore_rpc::{Client, RpcApi};
 
+#[derive(Clone)]
+struct ServerData {
+    btc_rpc: Arc<Client>,
+    config: utils::Config,
+}
+
 #[derive(Serialize, Deserialize)]
 struct GetBlockHashParams {
     height: u64,
 }
 
 async fn getblockhash(
-    data: Data<Arc<Client>>,
+    data: Data<Arc<ServerData>>,
     Params(params): Params<GetBlockHashParams>,
 ) -> Result<String, JsonRpcError> {
-    let client = data.as_ref();
+    let client = data.btc_rpc.as_ref();
 
     match client.get_block_hash(params.height) {
         Ok(block_hash) => Ok(block_hash.to_string()),
+        Err(e) => Err(JsonRpcError::Full {
+            code: -1,
+            message: e.to_string(),
+            data: None,
+        }),
+    }
+}
+
+async fn getblockcount(data: Data<Arc<ServerData>>) -> Result<u64, JsonRpcError> {
+    let client = data.btc_rpc.as_ref();
+
+    match client.get_block_count() {
+        Ok(block_count) => Ok(block_count),
         Err(e) => Err(JsonRpcError::Full {
             code: -1,
             message: e.to_string(),
@@ -48,17 +67,39 @@ fn make_bitcoincore_rpc() -> Arc<Client> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Init the bitcoincore_rpc client
+
     let btc_rpc = make_bitcoincore_rpc();
+
+    // Load the provider config
+
+    let config = match utils::load_config() {
+        Ok(config) => config,
+        Err(e) => {
+            eprintln!("Couldn't load provider config: {}", e);
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Couldn't load provider config",
+            ));
+        }
+    };
+
+    // Construct the JSON-RPC server
+
+    let server_data = Arc::new(ServerData { btc_rpc, config });
+
+    let rpc_server = jsonrpc_v2::Server::new()
+        .with_data(Data::new(server_data))
+        .with_method("getblockhash", getblockhash)
+        .with_method("getblockcount", getblockcount)
+        .finish();
+
+    // Start up the actix-web server
 
     let port = std::env::var("PROVIDER_PORT").unwrap_or_else(|_| "3030".to_string());
     println!("Server is running on http://127.0.0.1:{}", port);
 
     let addr = format!("127.0.0.1:{}", port);
-
-    let rpc_server = jsonrpc_v2::Server::new()
-        .with_data(Data::new(btc_rpc))
-        .with_method("getblockhash", getblockhash)
-        .finish();
 
     actix_web::HttpServer::new(move || {
         let rpc = rpc_server.clone();
