9918cd3b31a2dc3786fea87bd5fe5647126fac22
Nikola Ristić <ristic@risticnikola.com>
2025-04-18 14:39:07 +0900

    accept eth-api port in l2_block_checker helper script

--- internal/l2_block_checker.sh
+++ internal/l2_block_checker.sh
@@ -1,4 +1,7 @@
- while true; do
-     echo "Subnet block $(cast block-number --rpc-url http://localhost:8545)"
+#!/bin/bash
+PORT=${1:-8545}
+
+while true; do
+     echo "Subnet block $(cast block-number --rpc-url http://localhost:$PORT)"
      sleep 1
- done
+done
